8afd61042d7c8d2ae6905e697d70ac615874f390
elkanwang <wangxiangkang@gmail.com>
2015-04-02 23:46:55 -0400

    using angularjs ui router to make it one page application

---
+++ _site/scripts/mainCtrl.js
@@ -0,0 +1,4 @@
+myApp.controller('mainCtrl', function($scope) {
+        $scope.firstName= "John";
+        $scope.lastName= "Doe";
+    });
---
+++ _site/scripts/myApp.js
@@ -0,0 +1,36 @@
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise('/profile');
+
+  // Now set up the states
+  $stateProvider
+
+    .state('profile', {
+      url:'/profile',
+      templateUrl: "templates/profile.html"
+    })
+
+    .state('blog', {
+      url: "/blog",
+      templateUrl: "templates/blog.html"
+    })
+
+    .state('projects', {
+      url: "/projects",
+      templateUrl: "templates/projects.html"
+    })
+
+    .state('work', {
+      url: "/work",
+      templateUrl: "templates/work.html"
+    })
+
+    .state('courses', {
+      url: "/courses",
+      templateUrl: "templates/courses.html"
+    })
+
+});
